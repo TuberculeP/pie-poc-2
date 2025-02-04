594969106f83f0ccba2bae2d88584f4a30d45984
Félix Laviéville <lavieville.felix@gmail.com>
2025-02-04 17:44:04 +0100

    feat: saving final test

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
 "use client";
 
+import useFooBarHook from "@/hooks/fooBarHook";
+import { useEffect } from "react";
+
 export default function Home() {
+  const { fooBarData, fetchData } = useFooBarHook();
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div>
       <h1>Test</h1>
+      <pre>{JSON.stringify(fooBarData, null, 2)}</pre>
     </div>
   );
 }
