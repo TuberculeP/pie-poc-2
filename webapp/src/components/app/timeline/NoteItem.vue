a4c4d0cfa610e1b39395c35f93ea1337d1c678c6
Denis Bucspun <bucspun.d@gmail.com>
2025-07-04 16:42:29 +0200

    :sparkles: ajout de la grille, pattern déplaçable et resizable

---
+++ webapp/src/components/app/timeline/NoteItem.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="note">
+    {{ note.pitch }}
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+const props = defineProps<{
+  note: { i:string; pitch:string; start:number; duration:number }
+}>()
+</script>
+
+<style scoped>
+.note {
+  width: 100%;
+  height: 100%;
+  background: #ffa000;
+  border: 1px solid #ff8f00;
+  border-radius: 2px;
+  font-size: 10px;
+  color: #fff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  box-sizing: border-box;
+  cursor: grab;
+  user-select: none;
+}
+</style>
