912300a9bba3ab26f1835efcb20c2770d2ede28f
Félix Laviéville <lavieville.felix@gmail.com>
2025-07-22 16:15:09 +0200

    fix: adding log

--- server/src/config/db.config.ts
+++ server/src/config/db.config.ts
@@ -7,6 +7,12 @@ const { POSTGRES_URL } = process.env;
 
 let pg: DataSource;
 
+console.log(
+  "\x1b[44m%s\x1b[0m",
+  "server/src/config/db.config.ts:10 POSTGRES_URL",
+  POSTGRES_URL,
+);
+
 if (POSTGRES_URL) {
   pg = new DataSource({
     type: "postgres",
