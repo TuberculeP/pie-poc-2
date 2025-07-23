1285fbe4fd479d66e360ef6ff5985dfe838f88f4
Héloïse Le Lez <lelez.heloise@gmail.com>
2025-07-23 13:42:24 +0200

    feat : style bases

--- webapp/src/main.ts
+++ webapp/src/main.ts
@@ -3,7 +3,10 @@ import App from "./App.vue";
 import { router } from "./router";
 import { i18n } from "./lib/lang";
 import { createPinia } from "pinia";
+
 import "./reset.css";
+import "./styles/colors.css";
+import "./styles/base.css";
 
 const pinia = createPinia();
 
