151f0ce27f63ab4616d16dc0c7af9222d47c88b8
BDenisss <bucspun.d@gmail.com>
2025-07-24 18:50:09 +0200

    add MVP UI

--- webapp/src/router.ts
+++ webapp/src/router.ts
@@ -4,12 +4,13 @@ import LandingIndex from "./views/landing/LandingIndex.vue";
 
 // Bloob Webapp
 import BloopApp from "./views/app/BloopApp.vue";
-import GridTimeline from "./views/app/GridTimeline.vue";
+import TimelineView from "./views/TimelineView.vue";
+
 
 const routes = [
   { path: "/", component: LandingIndex },
   { path: "/app", component: BloopApp, name: "app-main" },
-  { path: "/grid", component: GridTimeline, name: "app-grid-timeline" },
+  { path: "/timeline", component: TimelineView, name: "timeline" },
 ];
 
 export const router = createRouter({
